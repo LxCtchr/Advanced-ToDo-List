948f61be93fa67d297d1c0ceb95da6d0956c0fa7
usinaleksandr801@gmail.com <79891938+LxCtchr@users.noreply.github.com>
2025-04-28 18:37:07 +0600

    Попытка пофиксить краш страницы при перезагрузке

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "_redirects",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
